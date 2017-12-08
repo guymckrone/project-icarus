5808d854a7bdff1317ab4daedd70279c12caba4a
gmcfeeters3345 <guy.mckrone@gmail.com>
2017-12-08 13:34:41 -0800

    Fixing error

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -124,6 +124,7 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
             playIntSec = 0;
             playIntHour++;
         }*/
+        System.out.println(ice);
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.setProjectionMatrix(camera.combined);
